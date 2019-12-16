acfa1b33340ac1745ca6e0fb3c060a24fb3ca9c7
Funcy <w963963555577@gmail.com>
2019-12-16 17:04:21 +0800

    no message

--- ShaderLibrary/VertexAnimation.hlsl
+++ ShaderLibrary/VertexAnimation.hlsl
@@ -14,13 +14,13 @@ float4 WindAnimation(float4 nv1)
 {
     float4 nv = mul(unity_ObjectToWorld, nv1);
     float4 objectOrigin = mul(unity_ObjectToWorld, float4(0, 0, 0, 1));
-    
+    float4 positionWS = mul(unity_ObjectToWorld, nv1);
     float4 positionMask = _PositionMask.SampleLevel(sampler_PositionMask, TRANSFORM_TEX(nv1.xy, _PositionMask), 0);
     float chanelMask = positionMask.r * positionMask.a;
     
     float _DistanceFromOrigin = distance(objectOrigin.y, nv.y);
-    float _anchored = sin((_Time.y + nv.x) * _Speed + nv.y * _Amount) * _Distance * (_DistanceFromOrigin / 3);
-    float _unanchored = sin((_Time.y + nv.z) * _Speed + nv.y * _Amount) * _Distance;
+    float _anchored = sin((_Time.y + nv.x + positionWS.x + positionWS.z) * _Speed + nv.y * _Amount) * _Distance * (_DistanceFromOrigin / 3);
+    float _unanchored = sin((_Time.y + nv.z + positionWS.x + positionWS.z) * _Speed + nv.y * _Amount) * _Distance;
     float _nxz = _ZMotion * sin(_Time.y * (_Speed / 10 * _ZMotionSpeed) + nv.y * _Amount * (_ZMotionSpeed / 10));
     nv.x += lerp(_unanchored, _anchored, _OriginWeight) * (1 - _nxz) * chanelMask;
     nv.z += lerp(_unanchored, _anchored, _OriginWeight) * (_nxz) * chanelMask;
