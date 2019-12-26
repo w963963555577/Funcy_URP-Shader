d12fd941b724c003dffbb4d35ba75fdb10252dc5
智樂堂_李宇凡 <w963963555577@gmail.com>
2019-12-26 17:17:49 +0800

    LWRP 樹的風動修正

--- ShaderLibrary/VertexAnimation.hlsl
+++ ShaderLibrary/VertexAnimation.hlsl
@@ -12,9 +12,9 @@ float _OriginWeight;
 half _DebugMask;
 float4 WindAnimation(float4 nv1)
 {
-    float4 nv = mul(unity_ObjectToWorld, nv1);
-    float4 objectOrigin = mul(unity_ObjectToWorld, float4(0, 0, 0, 1));
-    float4 positionWS = mul(unity_ObjectToWorld, nv1);
+    float4 nv = mul(GetObjectToWorldMatrix(), nv1);
+    float4 objectOrigin = mul(GetObjectToWorldMatrix(), float4(0, 0, 0, 1));
+    float4 positionWS = mul(GetObjectToWorldMatrix(), nv1);
     float4 positionMask = _PositionMask.SampleLevel(sampler_PositionMask, TRANSFORM_TEX(nv1.xy, _PositionMask), 0);
     float chanelMask = positionMask.r * positionMask.a;
     
@@ -24,6 +24,6 @@ float4 WindAnimation(float4 nv1)
     float _nxz = _ZMotion * sin(_Time.y * (_Speed / 10 * _ZMotionSpeed) + nv.y * _Amount * (_ZMotionSpeed / 10));
     nv.x += lerp(_unanchored, _anchored, _OriginWeight) * (1 - _nxz) * chanelMask;
     nv.z += lerp(_unanchored, _anchored, _OriginWeight) * (_nxz) * chanelMask;
-    nv1 = mul(unity_WorldToObject, nv);
+    nv1 = mul(GetWorldToObjectMatrix(), nv);
     return nv1;
 }
