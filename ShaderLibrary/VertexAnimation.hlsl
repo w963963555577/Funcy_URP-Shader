c9fbda84929780dd6b3a8e131cf63f3f72cea1bf
FuncyLee <w963963555577@gmail.com>
2019-12-31 23:39:16 +0800

    Update

--- ShaderLibrary/VertexAnimation.hlsl
+++ ShaderLibrary/VertexAnimation.hlsl
@@ -17,9 +17,25 @@ And use these line
 float4 positionMask = _PositionMask.SampleLevel(sampler_PositionMask, TRANSFORM_TEX(input.positionOS.xy, _PositionMask), 0);
 input.positionOS = WindAnimation(input.positionOS, _PositionMask_ST, _Speed, _Amount, _Distance, _ZMotion, _ZMotionSpeed, _OriginWeight, _DebugMask, positionMask);
 */
-float4 WindAnimation(float4 nv1, float4 _PositionMask_ST, float _Speed, float _Amount, float _Distance, float _ZMotion, float _ZMotionSpeed, float _OriginWeight, half _DebugMask, float4 positionMask)
+
+//WindAnimation
+float4 _PositionMask_ST;
+float _Speed;
+float _Amount;
+float _Distance;
+float _ZMotion;
+float _ZMotionSpeed;
+float _OriginWeight;
+
+half _DebugMask;
+
+TEXTURE2D(_PositionMask);       SAMPLER(sampler_PositionMask);
+
+float4 WindAnimation(float4 nv1)
 {
     float4 nv = mul(GetObjectToWorldMatrix(), nv1);
+    float4 positionMask = _PositionMask.SampleLevel(sampler_PositionMask, TRANSFORM_TEX(nv1.xy, _PositionMask), 0);
+    
     float4 objectOrigin = mul(GetObjectToWorldMatrix(), float4(0, 0, 0, 1));
     float4 positionWS = mul(GetObjectToWorldMatrix(), nv1);
     float chanelMask = positionMask.r * positionMask.a;
@@ -30,6 +46,7 @@ float4 WindAnimation(float4 nv1, float4 _PositionMask_ST, float _Speed, float _A
     float _nxz = _ZMotion * sin(_Time.y * (_Speed / 10 * _ZMotionSpeed) + nv.y * _Amount * (_ZMotionSpeed / 10));
     nv.x += lerp(_unanchored, _anchored, _OriginWeight) * (1 - _nxz) * chanelMask;
     nv.z += lerp(_unanchored, _anchored, _OriginWeight) * (_nxz) * chanelMask;
+    
     nv1 = mul(GetWorldToObjectMatrix(), nv);
     return nv1;
 }
