0e5b2f39b00953819159500c6819ee158375a118
智樂堂_李宇凡 <w963963555577@gmail.com>
2020-11-26 17:09:23 +0800

    更新 RenderFeature 腳本

--- Runtime/RenderFeature/MobileBloom/MobileBloom.cs
+++ Runtime/RenderFeature/MobileBloom/MobileBloom.cs
@@ -1,10 +1,35 @@
 
 using UnityEngine;
+using UnityEngine.Funcy.LWRP.Runtime;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
-namespace UnityEngine.Funcy.LWRP.Runtime
+
+
+public class MobileBloom : ScriptableRendererFeature
 {
-    internal class MobileBloomLwrpPass : ScriptableRenderPass
+    [System.Serializable]
+    public class MobileBloomSettings
+    {
+        public RenderPassEvent Event = RenderPassEvent.AfterRenderingTransparents;
+
+        public Material blitMaterial = null;
+
+        [Range(0, 5)]
+        public float BlurAmount = 1f;
+
+        public float BloomAmount = 1f;
+
+        public float BloomThreshold = 0.0f;
+
+        public float BloomAdd = 0.0f;
+
+        [Range(0, 1)]
+        public float OrigBlend = 1.0f;
+    }
+
+    public MobileBloomSettings settings = new MobileBloomSettings();
+
+    internal class MobileBloomPass : ScriptableRenderPass
     {
         public Material material;
 
@@ -32,7 +57,7 @@ namespace UnityEngine.Funcy.LWRP.Runtime
         static readonly int blurTexString = Shader.PropertyToID("_BlurTex");
         static readonly int tempCopyString = Shader.PropertyToID("_TempCopy");
 
-        public MobileBloomLwrpPass(RenderPassEvent renderPassEvent, Material material,
+        public MobileBloomPass(RenderPassEvent renderPassEvent, Material material,
             float blurAmount, float bloomAmount, float bloomThreshold, float bloomAdd, float origBlend, string tag)
         {
             this.renderPassEvent = renderPassEvent;
@@ -56,7 +81,7 @@ namespace UnityEngine.Funcy.LWRP.Runtime
             CommandBuffer cmd = CommandBufferPool.Get(tag);
             RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
             opaqueDesc.depthBufferBits = 0;
-            
+
             cmd.GetTemporaryRT(tempCopyString, opaqueDesc, FilterMode.Bilinear);
             cmd.CopyTexture(source, tempCopy);
 
@@ -64,7 +89,7 @@ namespace UnityEngine.Funcy.LWRP.Runtime
             cmd.SetGlobalFloat(blAmountString, bloomAmount);
             cmd.SetGlobalFloat(blThresholdString, bloomThreshold);
             cmd.SetGlobalFloat(blAddString, bloomAdd);
-            cmd.SetGlobalFloat(orBString, origBlend); 
+            cmd.SetGlobalFloat(orBString, origBlend);
 
             cmd.GetTemporaryRT(blurTempString, Screen.width / 4, Screen.height / 4, 0, FilterMode.Bilinear);
             cmd.GetTemporaryRT(blurTemp1String, Screen.width / 8, Screen.height / 8, 0, FilterMode.Bilinear);
@@ -72,9 +97,9 @@ namespace UnityEngine.Funcy.LWRP.Runtime
 
             Blit(cmd, tempCopy, blurTemp, material, 0);
             Blit(cmd, blurTemp, blurTemp1, material, 1);
-            Blit(cmd, blurTemp1, blurTex, material, 1);            
+            Blit(cmd, blurTemp1, blurTex, material, 1);
             cmd.Blit(tempCopy, source, material, 2);
-            
+
             context.ExecuteCommandBuffer(cmd);
 
             cmd.SetGlobalTexture("_ResultTex", tempCopy);
@@ -92,4 +117,18 @@ namespace UnityEngine.Funcy.LWRP.Runtime
 
     }
 
+
+    MobileBloomPass mobilePostProcessPass;
+
+    public override void Create()
+    {
+        mobilePostProcessPass = new MobileBloomPass(settings.Event, settings.blitMaterial, settings.BlurAmount, settings.BloomAmount, settings.BloomThreshold, settings.BloomAdd, settings.OrigBlend, this.name);
+    }
+
+    public override void AddRenderPasses(UnityEngine.Rendering.Universal.ScriptableRenderer renderer, ref UnityEngine.Rendering.Universal.RenderingData renderingData)
+    {
+        mobilePostProcessPass.Setup(renderer.cameraColorTarget);
+        renderer.EnqueuePass(mobilePostProcessPass);
+    }
 }
+
