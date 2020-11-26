6abcf64040bf22513ab32bcdef3ceacab319f332
智樂堂_李宇凡 <w963963555577@gmail.com>
2020-11-26 17:09:47 +0800

    更新複製RGB工具

--- Editor/MenuExtension.cs
+++ Editor/MenuExtension.cs
@@ -254,10 +254,10 @@ public class MenuExtension
                 var fbx = AssetDatabase.LoadAssetAtPath<GameObject>(path);                                
                 var mat = importer.GetExternalObjectMap().ToList()[0].Value as Material;
                 string[] split = Path.GetFileName(Path.GetDirectoryName(path)).Split('_');
-
+                
                 var avatarID = importer.sourceAvatar == null ? "-1" : importer.sourceAvatar.name.Split('_')[1];
-                var modelID = split[1];
-                var modelName = split[2];
+                var modelID = split.Length > 1 ? split[1] : "-1";
+                var modelName = split.Length > 2 ? split[2] : "<尚未命名>";                
 
                 s = string.Format("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}{0}{7}{0}{8}{0}{9}{0}{10}{0}{11}{0}{12}{0}{13}{0}{14}",
                     tab, modelName, avatarID, "", "", modelID, 1, 0, 0, 0, 0, 0, 0, 0, CopyCharacterShaderInfo(mat));
